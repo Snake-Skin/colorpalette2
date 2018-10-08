afa12acd1142aec2856f9910b97d76ea8b2ae888
Mason Gentry <gentry@Masons-iMac.local>
2018-10-07 22:17:39 -0400

    some message

--- src/App.js
+++ src/App.js
@@ -29,67 +29,67 @@ class Colors extends Component {
 
 class Palettes extends Component {
   render (){
-    const colorname = (document.getElementsByClassName);
-    
+   //const colorname = document.querySelector.className;
+
+
+  
     return (
-      <div className="Palettes">
-    <div id="blue">
-      <div className="bluecolor1"><p>rgba(2, 0, 175, 1)</p></div>
-      <div className="bluecolor2">rgba(26, 54, 255, 1)</div>
-      <div className="bluecolor3">rgba(4, 0, 255, 1)</div>
-      <div className="bluecolor4"><p>{colorname}</p></div>
-      <div className="bluecolor5"></div>
-      <div className="bluecolor6"></div>
+  <div className="colors">
+       <div id="red" className="palettes" onClick={this.favorite}>
+      <div className="redcolor1">{'.redcolor4'}</div>
+      <div className="redcolor2">{'.redcolor4'}</div>
+      <div className="redcolor3">{'.redcolor4'}</div>
+      <div className="redcolor4">{'.redcolor4'}</div>
+      <div className="redcolor5">{'.redcolor4'}</div>
+      <div className="redcolor6">{'.redcolor4'}</div>
     </div>
 
-   <ul id="red">
-      <li className="redcolor1"></li>
-      <li className="redcolor2"></li>
-      <li className="redcolor3"></li>
-      <li className="redcolor4"></li>
-      <li className="redcolor5"></li>
-      <li className="redcolor6"></li>
-    </ul>
+<div id="orange" className="palettes">
+      <div className="orangecolor1">{'.orangecolor4'}</div>
+      <div className="orangecolor2">{'.orangecolor4'}</div>
+      <div className="orangecolor3">{'.orangecolor4'}</div>
+      <div className="orangecolor4">{'.orangecolor4'}</div>
+      <div className="orangecolor5">{'.orangecolor4'}</div>
+      <div className="orangecolor6">{'.orangecolor4'}</div>
+    </div>
 
+   <div id="yellow" className="palettes">
+      <div className="yellowcolor1">{'.yellowcolor4'}</div>
+      <div className="yellowcolor2">{'.yellowcolor4'}</div>
+      <div className="yellowcolor3">{'.yellowcolor4'}</div>
+      <div className="yellowcolor4">{'.yellowcolor4'}</div>
+      <div className="yellowcolor5">{'.yellowcolor4'}</div>
+      <div className="yellowcolor6">{'.yellowcolor4'}</div>
+    </div>
 
-    <ul id="yellow">
-      <li className="yellowcolor1"></li>
-      <li className="yellowcolor2"></li>
-      <li className="yellowcolor3"></li>
-      <li className="yellowcolor4"></li>
-      <li className="yellowcolor5"></li>
-      <li className="yellowcolor6"></li>
-    </ul>
+<div id="green" className="palettes">
+      <div className="greencolor1">{'.greencolor4'}</div>
+      <div className="greencolor2">{'.greencolor4'}</div>
+      <div className="greencolor3">{'.greencolor4'}</div>
+      <div className="greencolor4">{'.greencolor4'}</div>
+      <div className="greencolor5">{'.greencolor4'}</div>
+      <div className="greencolor6">{'.greencolor4'}</div>
+    </div>
 
-    <ul id="orange">
-      <li className="orangecolor1"></li>
-      <li className="orangecolor2"></li>
-      <li className="orangecolor3"></li>
-      <li className="orangecolor4"></li>
-      <li className="orangecolor5"></li>
-      <li className="orangecolor6"></li>
-    </ul>
+    <div id="blue" className="palettes">
+      <div className="bluecolor1">{'.bluecolor4'}</div>
+      <div className="bluecolor2">{'.bluecolor4'}</div>
+      <div className="bluecolor3">{'.bluecolor4'}</div>
+      <div className="bluecolor4">{'.bluecolor4'}</div>
+      <div className="bluecolor5">{'.bluecolor4'}</div>
+      <div className="bluecolor6">{'.bluecolor4'}</div>
+    </div>
 
-    <ul id="green">
-      <li className="greencolor1"></li>
-      <li className="greencolor2"></li>
-      <li className="greencolor3"></li>
-      <li className="greencolor4"></li>
-      <li className="greencolor5"></li>
-      <li className="greencolor6"></li>
-    </ul>
-
-    <ul id="white">
-      <li className="whitecolor1"></li>
-      <li className="whitecolor2"></li>
-      <li className="whitecolor3"></li>
-      <li className="whitecolor4"></li>
-      <li className="whitecolor5"></li>
-      <li className="whitecolor6"></li>
-
-    </ul>
+    <div id="white" className="palettes">
+      <div className="whitecolor1">{'.whitecolor4'}</div>
+      <div className="whitecolor2">{'.whitecolor4'}</div>
+      <div className="whitecolor3">{'.whitecolor4'}</div>
+      <div className="whitecolor4">{'.whitecolor4'}</div>
+      <div className="whitecolor5">{'.whitecolor4'}</div>
+      <div className="whitecolor6">{'.whitecolor4'}</div>
+    </div>
         
-      </div>
+  </div>
     );
   }
 }
