357ff3c14db8b4ed34db36a7163f99acd9951272
Mason Gentry <gentry@Masons-iMac.local>
2018-10-10 00:24:37 -0400

    some message

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ import './index.css';
 
 class Colorpage extends Component {
   render (){
+    
     return (
-      <div className="container">
+      <div >
         <Colors />
       </div>
     );
@@ -19,9 +20,32 @@ class Colorpage extends Component {
 
 class Colors extends Component {
   render (){
+    
     return (
-      <div className="colors"> 
-        <Palettes />
+      <div >
+        <Favorites />
+      </div>
+    );
+  }
+}
+
+class Favorites extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {addClass: false};
+  }
+
+  toggle() {
+      this.setState({addClass: !this.state.addClass});
+  }
+  
+  render (){
+      let favoritesClass = ["favorites"]; if(this.state.addClass) {favoritesClass.push('.favorites');
+    }
+   
+    return (
+    <div className ={favoritesClass.join( ' ')} onClick={this.toggle.bind(this)}>
+    <Palettes />
       </div>
     );
   }
@@ -29,65 +53,102 @@ class Colors extends Component {
 
 class Palettes extends Component {
   render (){
-   //const colorname = document.querySelector.className;
+// ADD onCick that switches places border around color clicked and grows it
+const whiteColors = [
+  '#F0F7F4',
+  '#F7FFFB',
+  '#F4F9FF',
+  '#F9F6F4',
+  '#F9FAFF',
+  '#F9FDFF',
+];
+const orangeColors = [
+  '#FF6C2D',
+  '#FF970F',
+  '#FF8E32',
+  '#F46100',
+  '#F98500',
+  '#F98800',
+];
+const greenColors = [
+  '#4ECE6C',
+  '#4B7F52',
+  '#96E8BC',
+  '#B6F9C9',
+  '#C9FFE2',
+  '#C9FFE6',
+];
+const yellowColors = [
+  '#F3E37C',
+  '#F2DB48',
+  '#EDD642',
+  '#F2D937',
+  '#F2E8AF',
+  '#F2E8A7',
+];  
+const redColors = [
+  '#771828',
+  '#971220',
+  '#501820',
+  '#BE1930',
+  '#DD1D35',
+  '#DD213C',
+];
+const blueColors = [
+  '#0200AF',
+  '#1A36FF',
+  '#0400FF',
+  '#002096',
+  '#5C08FF',
+  '#5A0DC8',
+];
+const palettebackground = '	#e6e6e6';
 
+return (
+  <div style={ {background: palettebackground} } className="colors">
 
-  
-    return (
-  <div className="colors">
-       <div id="red" className="palettes" onClick={this.favorite}>
-      <div className="redcolor1">{'.redcolor4'}</div>
-      <div className="redcolor2">{'.redcolor4'}</div>
-      <div className="redcolor3">{'.redcolor4'}</div>
-      <div className="redcolor4">{'.redcolor4'}</div>
-      <div className="redcolor5">{'.redcolor4'}</div>
-      <div className="redcolor6">{'.redcolor4'}</div>
-    </div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: redColors[0]} }>{ redColors[0] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: redColors[1]} }>{ redColors[1] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: redColors[2]} }>{ redColors[2] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: redColors[3]} }>{ redColors[3] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: redColors[4]} }>{ redColors[4] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: redColors[5]} }>{ redColors[5] }</div>
 
-<div id="orange" className="palettes">
-      <div className="orangecolor1">{'.orangecolor4'}</div>
-      <div className="orangecolor2">{'.orangecolor4'}</div>
-      <div className="orangecolor3">{'.orangecolor4'}</div>
-      <div className="orangecolor4">{'.orangecolor4'}</div>
-      <div className="orangecolor5">{'.orangecolor4'}</div>
-      <div className="orangecolor6">{'.orangecolor4'}</div>
-    </div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: orangeColors[0]} }>{ orangeColors[0] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: orangeColors[1]} }>{ orangeColors[1] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: orangeColors[2]} }>{ orangeColors[2] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: orangeColors[3]} }>{ orangeColors[3] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: orangeColors[4]} }>{ orangeColors[4] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: orangeColors[5]} }>{ orangeColors[5] }</div>
+   
+      <div className="palettes" style={ {width: 100, height: 100, background: yellowColors[0]} }>{ yellowColors[0] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: yellowColors[1]} }>{ yellowColors[1] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: yellowColors[2]} }>{ yellowColors[2] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: yellowColors[3]} }>{ yellowColors[3] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: yellowColors[4]} }>{ yellowColors[4] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: yellowColors[5]} }>{ yellowColors[5] }</div>
 
-   <div id="yellow" className="palettes">
-      <div className="yellowcolor1">{'.yellowcolor4'}</div>
-      <div className="yellowcolor2">{'.yellowcolor4'}</div>
-      <div className="yellowcolor3">{'.yellowcolor4'}</div>
-      <div className="yellowcolor4">{'.yellowcolor4'}</div>
-      <div className="yellowcolor5">{'.yellowcolor4'}</div>
-      <div className="yellowcolor6">{'.yellowcolor4'}</div>
-    </div>
+      <div className="palettes" style={ {width: 100, height: 100, background: greenColors[0]} }>{ greenColors[0] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: greenColors[1]} }>{ greenColors[1] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: greenColors[2]} }>{ greenColors[2] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: greenColors[3]} }>{ greenColors[3] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: greenColors[4]} }>{ greenColors[4] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: greenColors[5]} }>{ greenColors[5] }</div>
 
-<div id="green" className="palettes">
-      <div className="greencolor1">{'.greencolor4'}</div>
-      <div className="greencolor2">{'.greencolor4'}</div>
-      <div className="greencolor3">{'.greencolor4'}</div>
-      <div className="greencolor4">{'.greencolor4'}</div>
-      <div className="greencolor5">{'.greencolor4'}</div>
-      <div className="greencolor6">{'.greencolor4'}</div>
-    </div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: blueColors[0]} }>{ blueColors[0] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: blueColors[1]} }>{ blueColors[1] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: blueColors[2]} }>{ blueColors[2] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: blueColors[3]} }>{ blueColors[3] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: blueColors[4]} }>{ blueColors[4] }</div>
+      <div className="paletteswhiteletters" style={ {width: 100, height: 100, background: blueColors[5]} }>{ blueColors[5] }</div>
 
-    <div id="blue" className="palettes">
-      <div className="bluecolor1">{'.bluecolor4'}</div>
-      <div className="bluecolor2">{'.bluecolor4'}</div>
-      <div className="bluecolor3">{'.bluecolor4'}</div>
-      <div className="bluecolor4">{'.bluecolor4'}</div>
-      <div className="bluecolor5">{'.bluecolor4'}</div>
-      <div className="bluecolor6">{'.bluecolor4'}</div>
-    </div>
+      <div className="palettes" style={ {width: 100, height: 100, background: whiteColors[0]} }>{ whiteColors[0] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: whiteColors[1]} }>{ whiteColors[1] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: whiteColors[2]} }>{ whiteColors[2] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: whiteColors[3]} }>{ whiteColors[3] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: whiteColors[4]} }>{ whiteColors[4] }</div>
+      <div className="palettes" style={ {width: 100, height: 100, background: whiteColors[5]} }>{ whiteColors[5] }</div>
 
-    <div id="white" className="palettes">
-      <div className="whitecolor1">{'.whitecolor4'}</div>
-      <div className="whitecolor2">{'.whitecolor4'}</div>
-      <div className="whitecolor3">{'.whitecolor4'}</div>
-      <div className="whitecolor4">{'.whitecolor4'}</div>
-      <div className="whitecolor5">{'.whitecolor4'}</div>
-      <div className="whitecolor6">{'.whitecolor4'}</div>
-    </div>
         
   </div>
     );
